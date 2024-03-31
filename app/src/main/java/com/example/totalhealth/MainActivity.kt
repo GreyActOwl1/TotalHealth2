45c82123f285580a636127d021d97e3c682fc815
GreyActOwl1 <106501057+GreyActOwl1@users.noreply.github.com>
2024-03-31 12:24:22 -0400

    Refactor MainActivity

--- app/src/main/java/com/example/totalhealth/MainActivity.kt
+++ app/src/main/java/com/example/totalhealth/MainActivity.kt
@@ -19,24 +19,25 @@ import kotlinx.coroutines.launch
 class MainActivity : AppCompatActivity() {
 
     private lateinit var foodItemsRecyclerView: RecyclerView
-//    private lateinit var foodItemsAdapter: FoodItemsAdapter
     private lateinit var totalCaloriesTextView: TextView
     private lateinit var totalWaterTextView: TextView
-    private lateinit var foodItems: MutableList<FoodItemEntity>
+    private val foodItems = mutableListOf<FoodItemEntity>()
     private var totalWaterIntake: Int = 0
     private var totalCalories: Int = 0
-
-
-
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        setupUI()
+        updateDisplays()
+        addButtonListeners()
+    }
+
+    private fun setupUI() {
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { view, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            view.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
 
@@ -44,11 +45,6 @@ class MainActivity : AppCompatActivity() {
         totalWaterTextView = findViewById(R.id.total_water_intake_text)
         totalCaloriesTextView.text = getString(R.string.calories, totalCalories)
         totalWaterTextView.text = getString(R.string.water, totalWaterIntake)
-
-        foodItems = mutableListOf()
-
-
-
 
         foodItemsRecyclerView = findViewById<RecyclerView>(R.id.recycler_view_food_items).apply {
             adapter = FoodItemsAdapter(foodItems)
@@ -60,47 +56,40 @@ class MainActivity : AppCompatActivity() {
                 )
             )
         }
+    }
 
-        lifecycleScope.launch() {
-            // Get and display all food items from database
-            (application as HealthApplication).db.foodItemDao().getAll().collect { items ->
-                foodItems.clear()
-                foodItems.addAll(items)
-                foodItemsRecyclerView.adapter?.notifyItemRangeChanged(0, foodItems.size)
-                Log.d("MainActivity", "Food Displays Updated")
 
+    private fun updateDisplays() {
+        val db = (application as HealthApplication).db
+        lifecycleScope.launch {
+            launch {
+                db.foodItemDao().getAll().collect { items ->
+                    foodItems.clear()
+                    foodItems.addAll(items)
+                    foodItemsRecyclerView.adapter?.notifyItemRangeChanged(0, items.size)
+                }
             }
-        }
-         lifecycleScope.launch {
-             //Update total calories intake
-             (application as HealthApplication).db.foodItemDao().getTotalCalories().collect {
-                 totalCalories = it ?: 0
-                 totalCaloriesTextView.text = getString(R.string.calories, totalCalories)
-                 Log.d("MainActivity", "Total Calories Updated${totalCalories}")
-             }
-         }
-lifecycleScope.launch {(application as HealthApplication).db.waterIntakeEventDao().getTotalIntake().collect {
-    totalWaterIntake = it ?: 0
-    totalWaterTextView.text = getString(R.string.water, totalWaterIntake)
-    Log.d("MainActivity", "Total Water Intake Updated${totalWaterIntake}")
-}}
-            //Update total water intake
-
-
-
-
-
-
-
-        findViewById<Button>(R.id.button_add_food).setOnClickListener {
-            startActivity(Intent(this, AddFoodItemActivity::class.java))
-        }
-
-        findViewById<Button>(R.id.button_add_water).setOnClickListener {
-//            Toast.makeText(this, "Add water entry functionality", Toast.LENGTH_SHORT).show()
-            startActivity(Intent(this, AddWaterEntryActivity::class.java))
+            launch {   db.foodItemDao().getTotalCalories().collect {
+                totalCalories = it ?: 0
+                totalCaloriesTextView.text = getString(R.string.calories, totalCalories)
+            }}
+            launch {  db.waterIntakeEventDao().getTotalIntake().collect {
+                totalWaterIntake = it ?: 0
+                totalWaterTextView.text = getString(R.string.water, totalWaterIntake)
+            } }
         }
     }
 
 
+
+
+
+    private fun addButtonListeners() {
+        findViewById<Button>(R.id.button_add_food).setOnClickListener {
+            startActivity(Intent(this, AddFoodItemActivity::class.java))
+        }
+        findViewById<Button>(R.id.button_add_water).setOnClickListener {
+            startActivity(Intent(this, AddWaterEntryActivity::class.java))
+        }
+    }
 }
