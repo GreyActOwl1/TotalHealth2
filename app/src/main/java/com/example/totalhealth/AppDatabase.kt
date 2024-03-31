3415123db282b5739f6c6c79c7c851e7e9c57689
GreyActOwl1 <106501057+GreyActOwl1@users.noreply.github.com>
2024-03-31 13:03:50 -0400

    Add Styles and Sample DB entries for Demo

--- app/src/main/java/com/example/totalhealth/AppDatabase.kt
+++ app/src/main/java/com/example/totalhealth/AppDatabase.kt
@@ -5,6 +5,10 @@ import androidx.room.Database
 import androidx.room.Room
 import androidx.room.RoomDatabase
 import androidx.room.TypeConverters
+import androidx.sqlite.db.SupportSQLiteDatabase
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers.IO
+import kotlinx.coroutines.launch
 
 @Database(entities = [FoodItemEntity::class, WaterIntakeEvent::class], version = 1)
 @TypeConverters(LocalDateTimeConverter::class)
@@ -16,16 +20,42 @@ abstract class AppDatabase : RoomDatabase() {
         @Volatile
         private var INSTANCE: AppDatabase? = null
 
+        val sampleFoodItems = listOf(
+            FoodItemEntity(name = "Apple", calories = 95),
+            FoodItemEntity(name = "Banana", calories = 105),
+            FoodItemEntity(name = "Carrot", calories = 25),
+            FoodItemEntity(name = "Broccoli", calories = 55),
+            FoodItemEntity(name = "Blueberries", calories = 84),
+            FoodItemEntity(name = "Almonds", calories = 164),
+            FoodItemEntity(name = "Chicken Breast", calories = 165),
+            FoodItemEntity(name = "Brown Rice", calories = 216),
+            FoodItemEntity(name = "Avocado", calories = 322),
+            FoodItemEntity(name = "Salmon", calories = 206),
+            FoodItemEntity(name = "Eggs", calories = 100),
+        )
+
+        private val roomDatabaseCallback = object : Callback() {
+            override fun onCreate(db: SupportSQLiteDatabase) {
+                super.onCreate(db)
+                CoroutineScope(IO).launch {
+                    INSTANCE?.foodItemDao()?.insertAll(sampleFoodItems)
+                }
+            }
+        }
+
         fun getInstance(context: Context): AppDatabase =
             INSTANCE ?: synchronized(this) {
                 INSTANCE ?: buildDatabase(context).also { INSTANCE = it }
             }
 
+
         private fun buildDatabase(context: Context) =
             Room.databaseBuilder(
                 context.applicationContext,
                 AppDatabase::class.java, "Health-Total-db"
-            ).build()
+            )
+                .addCallback(roomDatabaseCallback)
+                .build()
     }
 }
 
