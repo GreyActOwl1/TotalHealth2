e1209c9d6322b5d8ffd67782587f4d1e1bd0f4f9
GreyActOwl1 <106501057+GreyActOwl1@users.noreply.github.com>
2024-04-04 22:55:15 -0400

    Remove unused KAPT

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -2,7 +2,7 @@ plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.jetbrains.kotlin.android)
     id("com.google.devtools.ksp") version "1.9.21-1.0.15"
-    kotlin("kapt")
+
 
 
 }
