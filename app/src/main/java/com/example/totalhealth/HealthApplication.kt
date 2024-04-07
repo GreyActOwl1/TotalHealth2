037ba2d3a6308c122ec9d327328d6eb24c3b17e6
GreyActOwl1 <106501057+GreyActOwl1@users.noreply.github.com>
2024-04-07 04:28:29 -0600

    Add Notification Channel

--- app/src/main/java/com/example/totalhealth/HealthApplication.kt
+++ app/src/main/java/com/example/totalhealth/HealthApplication.kt
@@ -1,7 +1,33 @@
 package com.example.totalhealth
 
 import android.app.Application
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.content.Context
+import android.os.Build
 
 class HealthApplication : Application() {
     val db by lazy { AppDatabase.getInstance(this) }
+    override fun onCreate() {
+        super.onCreate()
+        createNotificationChannel()
+    }
+
+    private fun createNotificationChannel() {
+        // Create the NotificationChannel, but only on API 26+ because
+        // the NotificationChannel class is not in the Support Library.
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            val name = getString(R.string.channel_name)
+            val descriptionText = getString(R.string.channel_description)
+            val importance = NotificationManager.IMPORTANCE_DEFAULT
+            val CHANNEL_ID = "total_health_channel"
+            val channel = NotificationChannel(CHANNEL_ID, name, importance).apply {
+                description = descriptionText
+            }
+            // Register the channel with the system.
+            val notificationManager: NotificationManager =
+                getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+            notificationManager.createNotificationChannel(channel)
+        }
+    }
 }
